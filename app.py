b2dab1dcd401dd94c125a7eccb1f24a366ee0d5f
Alexandre Adolphe <aadolphe@simplon.co>
2026-03-10 17:58:34 +0400

    improve ui

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import streamlit as st
 import requests
 from loguru import logger
 
-text = st.text_input("Enter text to analyze sentiment:")
+st.title ("Analyse de sentiment")
+
+text = st.text_input("Entrer un texte pour analyser son sentiment:")
+
+st.divider()
 
 if text:
     logger.info(f"Text to analyse: {text}")
@@ -14,17 +18,31 @@ if text:
 
         sentiment_scores = response.json()
 
-        st.write("Résultat de l'analyse de sentiment:")
-        st.write(f"Polarité positive: {sentiment_scores['pos']}")
-        st.write(f"Polarité négative: {sentiment_scores['neg']}")
-        st.write(f"Polarité neutre: {sentiment_scores['neu']}")
-        st.write(f"Score composé: {sentiment_scores['compound']}")
+        st.header("Résultat de l'analyse de sentiment:")
+        
+        col1, col2 = st.columns(2);
+
+        with col1:
+            st.subheader(f"Polarité positive 🙂")
+            st.badge(f"{sentiment_scores['pos']}", color="green")
+
+            st.subheader(f"Polarité négative 🙁")
+            st.badge(f"{sentiment_scores['neg']}", color="red")
+
+        with col2:
+            st.subheader(f"Polarité neutre 😐")
+            st.badge(f"{sentiment_scores['neu']}", color="gray")
+
+            st.subheader(f"Score composé 🎯")
+            st.badge(f"{sentiment_scores['compound']}", color="blue")
+        
+        st.divider()
 
         if sentiment_scores['compound'] >= 0.05:
-            st.write("Sentiment global: Positif 🙂")
+            st.success("Sentiment global: Positif 🙂")
         elif sentiment_scores['compound'] <= -0.05:
-            st.write("Sentiment global: Négatif 🙁")
-        else:    st.write("Sentiment global: Neutre 😐")
+            st.error("Sentiment global: Négatif 🙁")
+        else:    st.info("Sentiment global: Neutre 😐")
 
     except requests.exceptions.RequestException as e:
         st.error(f"Error connecting to the API: {e}")
